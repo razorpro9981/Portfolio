784173c168908235917a7531e4915d368bb04a90
razorpro9981 <67879848+razorpro9981@users.noreply.github.com>
2023-08-16 13:37:46 +0000

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -29,6 +29,18 @@ function Hero() {
       >
         Ebenezer Kissiedu
       </motion.h2>
+      <motion.h1
+        className="hero-title"
+        initial={{ opacity: 0, y: 5 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.3,
+          ease: "easeInOut",
+          delay: 0.6,
+        }}
+      >
+        I am a
+      </motion.h1>
       <motion.h3
         className="hero-title-large hero-title-sub"
         initial={{ opacity: 0, y: 5 }}
@@ -39,7 +51,7 @@ function Hero() {
           delay: 1.05,
         }}
       >
-       I am a Software Engineer
+       Software Engineer
       </motion.h3>
       <motion.p
         className="hero-text"
